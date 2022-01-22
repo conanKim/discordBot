737f359a8640926bd5ada93c7c6d44e4cbbd75b2
conan <conan3511.dev@gmail.com>
2022-01-22 12:31:41 +0900

    명령어 prefix를 !로 변경

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, Intents } = require("discord.js");
+const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGES,
+    ],
 });
 
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+    console.log("Ready!");
+});
 
-client.on('message', (message) => {
-    console.log(message)
-})
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+client.on("messageCreate", (message) => {
+    const isCommand = (keyword = "") => {
+        const prefix = "!";
+        return message.content.startsWith(`${prefix}${keyword}`);
+    };
 
-	const { commandName } = interaction;
+    // Exit and stop if it's not there
+    if (!isCommand()) return;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	}
+    // The back ticks are Template Literals introduced in Javascript in ES6 or ES2015, as an replacement for String Concatenation Read them up here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+    if (isCommand("ping")) {
+        message.channel.send("pong!");
+    } else if (isCommand("foo")) {
+        message.channel.send("bar!");
+    }
 });
 
 // Login to Discord with your client's token
