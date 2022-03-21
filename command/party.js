76b38a355d5f6837b752593868ca6f7f871d40ff
conan <conan3511.dev@gmail.com>
2022-03-21 18:30:50 +0900

    party help message 수정

--- command/party.js
+++ command/party.js
@@ -6,9 +6,14 @@ const getParty = async ([keyword, ...param] = []) => {
     let emptyMsg = "";
     emptyMsg += `사용법\n`;
     emptyMsg += `!파티 생성 [레이드] [난이도]\n`;
-    emptyMsg += `!파티 조회\n`;
-    emptyMsg += `!파티 [레이드]\n`;
-    emptyMsg += `!파티 삭제 [닉네임]\n`;
+    emptyMsg += `!파티 목록\n`;
+    emptyMsg += `!파티 목록 [레이드]\n`;
+    emptyMsg += `!파티 삭제 [닉네임]\n\n`;
+
+    emptyMsg += `!파티 참가 [번호] [캐릭명]\n`;
+    emptyMsg += `!파티 조회 [번호]\n`;
+    emptyMsg += `!파티 조회 [캐릭명]\n`;
+    emptyMsg += `!파티 탈퇴 [번호] [캐릭명]\n`;
 
     let query;
 
@@ -23,7 +28,7 @@ const getParty = async ([keyword, ...param] = []) => {
             .catch(() => "파티 생성에 실패했습니다.");
     }
 
-    if (keyword === "조회") {
+    if (keyword === "목록") {
         query = !param[0] ? partyDao.list : partyDao.listByRaid;
         return pgClient
             .query(query, param)
@@ -45,7 +50,7 @@ const getParty = async ([keyword, ...param] = []) => {
             .catch(() => "파티 참가에 실패했습니다.");
     }
 
-    if (keyword === "멤버") {
+    if (keyword === "조회") {
         query = `${parseInt(param[0])}` === param[0] ? partyMemberDao.listByParty : partyMemberDao.listByChar;
 
         return pgClient
