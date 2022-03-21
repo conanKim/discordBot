2c52e720dbaa70bdcd85487f1c0aa6fa762cb40a
conan <conan3511.dev@gmail.com>
2022-03-21 13:34:14 +0900

    update typo

--- dao/character.js
+++ dao/character.js
@@ -4,8 +4,13 @@ const createTable = `CREATE TABLE IF NOT EXISTS characters (
     name varchar (50) unique not null,
     FOREIGN KEY (user_id) REFERENCES users (user_id),
     PRIMARY KEY (char_id, user_id)
-);`
+);`;
+
+const create = `INSERT INTO users (name, prefix) VALUES ($1, $2);`;
+const select = `SELECT * FROM users;`;
+const updatePrefix = `UPDATE users SET prefix = $1 WHERE name = $2;`;
+const deleteUser = `DELETE FROM users WHERE name = $1`;
 
 module.exports = {
-    init: createTable
-}
+    init: createTable,
+};
--- dao/user.js
+++ dao/user.js
@@ -1,17 +1,17 @@
-const createTable = `CREATE TABLE IF NOT EXISTS users (
+const INIT = `CREATE TABLE IF NOT EXISTS users (
     user_id serial PRIMARY KEY,
     name varchar (50) unique not null,
     prefix varchar (1) unique not null
-);`
-const create = `INSERT INTO users (name, prefix) VALUES ($1, $2);`
-const select = `SELECT * FROM users;`
-const updatePrefix = `UPDATE users SET prefix = $1 WHERE name = $2;`
-const deleteUser = `DELETE FROM users WHERE name = $1`
+);`;
+const CREATE = `INSERT INTO users (name, prefix) VALUES ($1, $2);`;
+const SELECT = `SELECT * FROM users;`;
+const UPDATE = `UPDATE users SET prefix = $1 WHERE name = $2;`;
+const DELETE = `DELETE FROM users WHERE name = $1`;
 
 module.exports = {
-    init: createTable,
-    create: create,
-    list: select,
-    updatePrefix: updatePrefix,
-    delete: deleteUser,
-}
+    init: INIT,
+    create: CREATE,
+    list: SELECT,
+    updatePrefix: UPDATE,
+    delete: DELETE,
+};
