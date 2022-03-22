3931f564f6f21e7e4bb1f07eec337a498449cea6
conan <conan3511.dev@gmail.com>
2022-03-22 21:43:17 +0900

    에러 핸들링 추가

--- index.js
+++ index.js
@@ -35,108 +35,112 @@ bot.once("ready", () => {
 PG.connect();
 
 bot.on("messageCreate", async (message) => {
-    console.log(message);
-    if (message.author.id === clientId) return;
-    if (!allowChannelId.includes(message.channelId)) return;
+    try {
+        console.log(message);
+        if (message.author.id === clientId) return;
+        if (!allowChannelId.includes(message.channelId)) return;
 
-    const isCommand = (keyword = "") => {
-        const prefix = "!";
-        return message.content.startsWith(`${prefix}${keyword}`);
-    };
+        const isCommand = (keyword = "") => {
+            const prefix = "!";
+            return message.content.startsWith(`${prefix}${keyword}`);
+        };
 
-    const sendMessage = (str = "") => {
-        message.reply(str);
-    };
+        const sendMessage = (str = "") => {
+            message.reply(str);
+        };
 
-    // Exit and stop if it's not there
-    if (!isCommand()) return;
+        // Exit and stop if it's not there
+        if (!isCommand()) return;
 
-    const [keyword, ...param] = message.content.split(" ");
+        const [keyword, ...param] = message.content.split(" ");
 
-    if (`!${parseInt(keyword.substring(1))}` === keyword) {
-        sendMessage(calcDutchPay([parseInt(keyword.substring(1))]));
-        return;
-    }
+        if (`!${parseInt(keyword.substring(1))}` === keyword) {
+            sendMessage(calcDutchPay([parseInt(keyword.substring(1))]));
+            return;
+        }
 
-    switch (keyword) {
-        case "!ping":
-            sendMessage("pong!");
-            break;
+        switch (keyword) {
+            case "!ping":
+                sendMessage("pong!");
+                break;
 
-        case "!help":
-        case "!command":
-        case "!명령어":
-            let command = "!주사위 [범위]\n";
-            command += "!쌀 [금액]\n";
-            command += "!레벨 [이름]\n";
-            command += "!골드\n";
-            command += "!숙제\n";
-            sendMessage(command);
-            break;
+            case "!help":
+            case "!command":
+            case "!명령어":
+                let command = "!주사위 [범위]\n";
+                command += "!쌀 [금액]\n";
+                command += "!레벨 [이름]\n";
+                command += "!골드\n";
+                command += "!숙제\n";
+                sendMessage(command);
+                break;
 
-        case "!쌀":
-        case "!n":
-        case "!N":
-        case "!ㅜ":
-            sendMessage(calcDutchPay(param));
-            break;
+            case "!쌀":
+            case "!n":
+            case "!N":
+            case "!ㅜ":
+                sendMessage(calcDutchPay(param));
+                break;
 
-        case "!주사위":
-            const max = param[0] || 100;
-            sendMessage(`주사위 ${max}\n결과 : ${Math.floor(Math.random() * max)}`);
-            break;
+            case "!주사위":
+                const max = param[0] || 100;
+                sendMessage(`주사위 ${max}\n결과 : ${Math.floor(Math.random() * max)}`);
+                break;
 
-        case "!멤버":
-            sendMessage(await getMember(param));
-            break;
+            case "!멤버":
+                sendMessage(await getMember(param));
+                break;
 
-        case "!캐릭터":
-            sendMessage(await getCharacter(param));
-            break;
+            case "!캐릭터":
+                sendMessage(await getCharacter(param));
+                break;
 
-        case "!파티":
-            sendMessage(await getParty(param));
-            break;
+            case "!파티":
+                sendMessage(await getParty(param));
+                break;
 
-        case "!레벨":
-            const msg = await getUserLevel(param[0]);
-            sendMessage(msg);
-            break;
+            case "!레벨":
+                const msg = await getUserLevel(param[0]);
+                sendMessage(msg);
+                break;
 
-        case "!골드":
-            sendMessage(getGoldEmbed());
-            break;
+            case "!골드":
+                sendMessage(getGoldEmbed());
+                break;
 
-        case "!풀숨":
-            sendMessage(getBreath(param));
-            break;
+            case "!풀숨":
+                sendMessage(getBreath(param));
+                break;
 
-        case "!풀숨설정":
-            sendMessage(setBreath(param));
-            break;
+            case "!풀숨설정":
+                sendMessage(setBreath(param));
+                break;
 
-        case "!숙제":
-            sendMessage(getHomework());
-            break;
+            case "!숙제":
+                sendMessage(getHomework());
+                break;
 
-        case "!문상":
-            sendMessage(parseCultureCoupon(param));
-            break;
+            case "!문상":
+                sendMessage(parseCultureCoupon(param));
+                break;
 
-        case "!레이드":
-            const raidMessage = await joinRaid(param);
-            sendMessage(raidMessage);
-            break;
+            case "!레이드":
+                const raidMessage = await joinRaid(param);
+                sendMessage(raidMessage);
+                break;
 
-        case "!관리자":
-            if (message.author.id !== adminId) return sendMessage("권한이 없습니다.");
+            case "!관리자":
+                if (message.author.id !== adminId) return sendMessage("권한이 없습니다.");
 
-            const resultMsg = await adminCommand(param);
-            sendMessage(resultMsg);
-            break;
+                const resultMsg = await adminCommand(param);
+                sendMessage(resultMsg);
+                break;
 
-        default:
-            sendMessage("존재하지 않는 명령어 입니다.");
+            default:
+                sendMessage("존재하지 않는 명령어 입니다.");
+        }
+    } catch (e) {
+        sendMessage("알수 없는 에러");
     }
 });
 
