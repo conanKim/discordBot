b18fa0ed78f7712a29aa6a9f21cf78aa339d8e90
conan <conan3511.dev@gmail.com>
2024-06-02 00:12:32 +0900

    리그 참가 시 id에 unique 속성 제거

--- src/dao/entry.js
+++ src/dao/entry.js
@@ -1,6 +1,6 @@
 const INIT = `CREATE TABLE IF NOT EXISTS entries (
-    league_id serial UNIQUE NOT NULL,
-    uma_uid varchar(50) UNIQUE NOT NULL,
+    league_id serial NOT NULL,
+    uma_uid varchar(50) NOT NULL,
     FOREIGN KEY (league_id) REFERENCES league (league_id),
     FOREIGN KEY (uma_uid) REFERENCES users (uma_uid),
     PRIMARY KEY (league_id, uma_uid)
