a5a339b9ba3339f4626dd8eef71b6c69ef6bdbfd
conan <conan3511.dev@gmail.com>
2024-06-01 23:06:44 +0900

    오타수정

--- src/dao/league.js
+++ src/dao/league.js
@@ -1,10 +1,10 @@
-const INIT = `CREATE TABLE IF NOT EXISTS users (
+const INIT = `CREATE TABLE IF NOT EXISTS league (
     league_id serial UNIQUE NOT NULL,
     league_name varchar(50) UNIQUE NOT NULL,
     league_date varchar(50) UNIQUE NOT NULL,
     join_date_limit varchar(50) UNIQUE NOT NULL,
     user_count_limit integer UNIQUE NOT NULL,
-    PRIMARY KEY (uma_uid)
+    PRIMARY KEY (league_id)
 );`;
 
 module.exports = {
