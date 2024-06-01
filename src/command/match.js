6c1cc7ab06ad91a91ae2c844eab4c9d1d3fe8039
conan <conan3511.dev@gmail.com>
2024-06-02 00:54:52 +0900

    lvup.gg 로 put api 날리는 기능 추가

--- src/command/match.js
+++ src/command/match.js
@@ -1,14 +1,45 @@
 const pgClient = require("../dao");
 const entryDao = require("../dao/entry");
 
-const create = async (param) => {
+const { putLvupGG } = require("../utils/utils");
+
+const create = async ([leagueName, token]) => {
     return pgClient
         .query(entryDao.select, param)
         .then((res) => {
             const body = {
-                rosters: res.map(entry => ({id: entry.uma_uid, name: entry.user_name}))
+               "easyBracketId": "665aa0d65b276900074cb1dd",
+               "organizeUserId": "665aa0bbbde5e230c677139d",
+               "title": "UGP 예선",
+               "status": "PRE",
+               "participantSize": 1,
+               "participantPerGroupSize": null,
+               "bracketType": "SINGLE_ELIMINATION",
+               "useOverlay": false,
+               "overlaySetting": {
+                 "version": "4",
+                 "rosters": res.map(entry => ({id: entry.uma_uid, name: entry.user_name})),
+                 "matchResults": [],
+                 "bracketType": "round-robin",
+                 "sortWeights": [],
+                 "useSemiFinal": false,
+                 "phase": "PRE",
+                 "roundsPlayed": 1,
+                 "timer": 0,
+                 "perGroup": 3,
+                 "roundRobinOrder": []
+               },
+               "useChannel": false,
+               "channelLinks": {},
+               "createdDatetime": 1717215446612,
+               "modifiedDatetime": 1717215446612,
+               "matchResults": [],
+               "pusher": {
+                 "type": "UPDATE",
+                 "id": "TVGX97An1epO4Jokt_9wP"
+               }
             }
-            return JSON.stringify(body)
+            return putLvupGG(body, token)
         })
         .catch(() => "실패");
 }
@@ -16,7 +47,7 @@ const create = async (param) => {
 const matchCommand = async ([keyword, ...param] = []) => {
     let emptyMsg = "";
     emptyMsg += `사용법\n`;
-    emptyMsg += `!대진표 생성 {리그명}\n`;
+    emptyMsg += `!대진표 생성 {리그명} {lvup.gg 토큰}\n`;
 
     if (!keyword) {
         return emptyMsg;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,9 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
+
+const putLvupGG = async (body, token) => {
+    return axios.put(`https://api.lvup.gg/v2/easy-brackets/665aa0d65b276900074cb1dd`, body, {headers: {Authorization: `Bearer ${token}`}});
+};
 
 module.exports = {
+    putLvupGG
 };
