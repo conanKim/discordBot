fa5119991a31f7df1de05d0587216ad58c75aefc
conan <conan3511.dev@gmail.com>
2024-06-02 00:09:33 +0900

    리그 참가 primary key 추가

--- src/dao/entry.js
+++ src/dao/entry.js
@@ -2,7 +2,8 @@ const INIT = `CREATE TABLE IF NOT EXISTS entries (
     league_id serial UNIQUE NOT NULL,
     uma_uid varchar(50) UNIQUE NOT NULL,
     FOREIGN KEY (league_id) REFERENCES league (league_id),
-    FOREIGN KEY (uma_uid) REFERENCES users (uma_uid)
+    FOREIGN KEY (uma_uid) REFERENCES users (uma_uid),
+    PRIMARY KEY (league_id, uma_uid)
 );`;
 
 const SELECT = `SELECT * FROM entries;`
