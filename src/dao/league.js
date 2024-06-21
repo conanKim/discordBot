f9a6b16f098048ce9d350e5f61e0c00c1bcf7496
conan <conan3511.dev@gmail.com>
2024-06-21 22:23:12 +0900

    리그 디비에 유니크 속성 제거

--- src/dao/league.js
+++ src/dao/league.js
@@ -1,9 +1,9 @@
 const INIT = `CREATE TABLE IF NOT EXISTS league (
     league_id serial UNIQUE NOT NULL,
     league_name varchar(50) UNIQUE NOT NULL,
-    league_date varchar(50) UNIQUE NOT NULL,
-    join_date_limit varchar(50) UNIQUE NOT NULL,
-    user_count_limit integer UNIQUE NOT NULL,
+    league_date varchar(50) NOT NULL,
+    join_date_limit varchar(50) NOT NULL,
+    user_count_limit integer NOT NULL,
     PRIMARY KEY (league_id)
 );`;
 
